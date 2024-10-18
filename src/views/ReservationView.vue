dd8172f5b398b85e778853567d30dc1d894955ed
Jonathan <jonathan.bosman@gmail.com>
2024-10-18 16:37:25 +0200

    front de la reseration finie, integration d'API en cours

--- src/views/ReservationView.vue
+++ src/views/ReservationView.vue
@@ -1,30 +1,47 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <div class="button"><span>⫯⫰⫯</span><span>Nombre de personnes</span><span>⫯⫰⫯</span></div>
-    <div class="select visible hidden" v-if="capacite && capacite.length && capacite[0].nombre_personnes">
+    <div class="button" @click="openPersonnes"><span>⫯⫰⫯</span><span>Nombre de personnes</span><span>⫯⫰⫯</span></div>
+    <div class="select visible" :class="{hidden : isOpenPersonnes===false}" v-if="capacite && capacite.length && capacite[0].nombre_personnes">
       <p v-if="isLoading">Chargement...</p>
-      <p class="options" :class="{selectedOpt : nombrePresonnes===i+1}" v-for="(cap, i) in capacite[0].nombre_personnes" @click="nombrePresonnes=i+1">{{ i+1 }}</p>
+      <p class="options" :class="{selectedOpt : nombrePersonnes===i+1}" v-for="(cap, i) in capacite[0].nombre_personnes" @click="nombrePersonnes=i+1">{{ i+1 }}</p>
     </div>
-    <div class="button"><span>⫯⫰⫯</span><span>Sélectionner la date</span><span>⫯⫰⫯</span></div>
-    <div class="visible hidden" v-if="capacite && capacite.length && capacite[0].nombre_personnes">
+    <div class="button" @click="openDate"><span>⫯⫰⫯</span><span>Sélectionner la date</span><span>⫯⫰⫯</span></div>
+    <div class="visible" :class="{hidden : isOpenDate===false}" v-if="capacite && capacite.length && capacite[0].nombre_personnes">
       <p v-if="isLoading">Chargement...</p>
       <VDatePicker :color="'orange'" @click="dateChange" v-model="date" />
     </div>
-    <div class="button"><span>⫯⫰⫯</span><span>Sélectionner l'heure</span><span>⫯⫰⫯</span></div>
+    <div class="button" @click="openHeure"><span>⫯⫰⫯</span><span>Sélectionner l'heure</span><span>⫯⫰⫯</span></div>
+    <div class="select visible" :class="{hidden : isOpenHeure===false}" v-if="capacite && capacite.length && capacite[0].nombre_personnes">
+      <p v-if="isLoading">Chargement...</p>
+      <p class="options" v-if="horairesJour.length<1">Aucune horaire pour cette date.</p>
+      <p class="options" :class="{selectedOpt : heureChoisie===heure}" v-for="(heure, i) in horairesJour" @click="heureChoisie=heure">{{ Math.floor((heure)/100).toString().length<2?'0'+Math.floor((heure)/100).toString():Math.floor((heure)/100).toString() }}h{{ (heure-Math.floor((heure)/100)*100).toString().length<2?(heure-Math.floor((heure)/100)*100).toString()+'0':(heure-Math.floor((heure)/100)*100).toString() }}</p>
+    </div>
     <div class="form">
       <label for="nom">
         <p>Nom :</p>
-        <input type="text" name="nom" id="nom">
+        <input v-model="nom" type="text" name="nom" id="nom">
       </label>
       <label for="prenom">
         <p>Prénom :</p>
-        <input type="text" name="prenom" id="prenom">
+        <input v-model="prenom" type="text" name="prenom" id="prenom">
+      </label>
+      <label for="email">
+        <p>E-mail :</p>
+        <input v-model="email" type="tel" name="email" id="email">
       </label>
       <label for="telephone">
         <p>Téléphone :</p>
-        <input type="tel" name="telephone" id="telephone">
+        <input v-model="telephone" type="tel" name="telephone" id="telephone">
       </label>
-      <button type="submit">summit dammit</button>
+      <label for="newsletter">
+        <p class="checkBox">J'accepte d'être tenu informé des nouveautés :</p>
+        <input v-model="newsletter" type="checkbox" name="newsletter" id="newsletter">
+      </label>
+      <label for="consent">
+        <p class="checkBox">J'ai lu et consent à la <RouterLink to="/confidentialite">Politique de confidentialité</RouterLink> et aux <RouterLink to="/conditions">CGU</RouterLink>RouterLink> :</p>
+        <input v-model="consent" type="checkbox" name="consent" id="consent">
+      </label>
+      <button :disabled="!consent" type="submit">Réserver</button>
     </div>
   </form>
 </template>
@@ -38,21 +55,81 @@ const reservations = computed(() => store.getters.allReservations);
 const capacite = computed(() => store.getters.allCapacite);
 const horaires = computed(() => store.getters.allHoraires);
 const isLoading = ref(true);
-const nombrePresonnes = ref(1);
+const isOpenPersonnes = ref(false);
+const isOpenDate = ref(false);
+const isOpenHeure = ref(false);
+const nom = ref('');
+const prenom = ref('');
+const email = ref('');
+const telephone = ref('');
+const newsletter = ref(false);
+const consent = ref(false);
+const nombrePersonnes = ref(1);
+const heureChoisie = ref(1200);
 const date = ref(new Date());
 const dateString = ref(`${date.value.getFullYear()}-${date.value.getMonth()}-${date.value.getDate()}`);
 const day = ref(date.value.getDay());
+const horairesJour = ref([]);
+
+const openPersonnes = () => {
+  isOpenPersonnes.value!==true?isOpenPersonnes.value=true:isOpenPersonnes.value=false;
+}
+const openDate = () => {
+  isOpenDate.value!==true?isOpenDate.value=true:isOpenDate.value=false;
+}
+const openHeure = () => {
+  isOpenHeure.value!==true?isOpenHeure.value=true:isOpenHeure.value=false;
+}
 
 const dateChange  = () => {
+  if(date.value===null)date.value = new Date();
   dateString.value = `${date.value.getFullYear()}-${date.value.getMonth()}-${date.value.getDate()}`;
   day.value = date.value.getDay()===0?7:date.value.getDay();
+  horairesJour.value = [];
+  for (let index = horaires.value[day.value-1].ouverture_matin; index <= horaires.value[day.value-1].fermeture_matin; index+=100) {
+    horairesJour.value.push(index);
+    if((index+15-Math.floor((index)/100)*100)<60 && index+15<horaires.value[day.value-1].fermeture_matin) {
+      horairesJour.value.push(index+15);
+    }
+    if((index+30-Math.floor((index)/100)*100)<60 && index+15<horaires.value[day.value-1].fermeture_matin) {
+      horairesJour.value.push(index+30);
+    }
+    if((index+45-Math.floor((index)/100)*100)<60 && index+15<horaires.value[day.value-1].fermeture_matin) {
+      horairesJour.value.push(index+45);
+    }
+  }
+  for (let index = horaires.value[day.value-1].ouverture_soir; index <= horaires.value[day.value-1].fermeture_soir; index+=100) {
+    horairesJour.value.push(index);
+    if((index+15-Math.floor((index)/100)*100)<60 && index+15<horaires.value[day.value-1].fermeture_soir) {
+      horairesJour.value.push(index+15);
+    }
+    if((index+30-Math.floor((index)/100)*100)<60 && index+15<horaires.value[day.value-1].fermeture_soir) {
+      horairesJour.value.push(index+30);
+    }
+    if((index+45-Math.floor((index)/100)*100)<60 && index+15<horaires.value[day.value-1].fermeture_soir) {
+      horairesJour.value.push(index+45);
+    }
+  }
 }
 
 const onSubmit = () => {
-  console.log(nombrePresonnes.value);
-  console.log(date.value.toString());
-  console.log(dateString.value);
-  console.log(day.value);
+  const user = {
+    nom: nom.value,
+    prenom: prenom.value,
+    email: email.value,
+    telephone: telephone.value,
+    newsletter: newsletter.value,
+  }
+  const reservation = {
+    telephone: telephone.value,
+    email: email.value,
+    date_reservation: dateString.value,
+    id_jour: day.value,
+    heure: heureChoisie.value,
+    nombre_personnes: nombrePersonnes.value,
+  }
+  console.log(user);
+  console.log(reservation);
 }
 
 onMounted(async () => {
@@ -90,6 +167,10 @@ form {
   border-radius: 12px;
 }
 
+.button>span:last-of-type {
+  color: #fff;
+}
+
 .form{
     display: flex;
     flex-direction: column;
@@ -114,6 +195,16 @@ form {
     width: 50%;
 }
 
+.form p.checkBox {
+    width: 100%;
+}
+
+.form input[type="checkbox"] {
+    width: auto;
+    border: none;
+    border-radius: 12px;
+}
+
 .form input, .form button {
     width: 100%;
     border: none;
@@ -121,9 +212,17 @@ form {
 }
 
 .form button {
-    width: auto;
-    background-color: white;
-    color: #6C9E41;
+  width: auto;
+  background-color: white;
+  color: #6C9E41;
+}
+
+.form button[disabled] {
+  background-color: #E3CBCB;
+}
+
+.form button:not([disabled]), .form input[type="checkbox"], .button {
+  cursor: pointer;
 }
 
 .select {
@@ -139,9 +238,9 @@ form {
   flex-direction: column;
 }
 
-// .hidden {
-//   display: none;
-// }
+.hidden {
+  display: none;
+}
 
 .options {
     color: #DE7B14;
